665a1f0e8d7a2d637141009570d12d22e34a275c
moyersjm <moyersjm@rose-hulman.edu>
2018-10-25 21:03:19 -0400

    Done

--- src/problem1.py
+++ src/problem1.py
@@ -2,9 +2,9 @@
 Exam 3, problem 1.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  October 2018.
+         their colleagues and Jonathan Moyers.  October 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -86,9 +86,13 @@ def problem1(numbers):
       :type numbers:  [int]
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # Done: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    total = 0
+    for k in range(len(numbers)//3, 2*len(numbers)//3):
+        total = total + numbers[k]
+    return total
 
 
 ###############################################################################
