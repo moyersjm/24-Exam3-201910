db494551ab9537f7c2eb558e52b4543e7269b441
moyersjm <moyersjm@rose-hulman.edu>
2018-10-25 21:07:27 -0400

    Done

--- src/problem2.py
+++ src/problem2.py
@@ -2,9 +2,9 @@
 Exam 3, problem 2.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  October 2018.
+         their colleagues and Jonathan Moyers.  October 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -90,9 +90,16 @@ def problem2(sequence):
       :type sequence [list]
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # Done: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    largest = 0
+    index = 0
+    for k in range(len(sequence)):
+        if abs(sequence[k]) > largest:
+            largest = abs(sequence[k])
+            index = k
+    return index
 
 
 ###############################################################################
